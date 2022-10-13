016fe699f689b9c285cf752c940929f4d387333d
SATHISHKUMAR-01 <sathishprakash233@gmail.com>
2022-10-13 16:06:15 +0530

    Added Navigation page

---
+++ navigation.php
@@ -0,0 +1,13 @@
+
+
+<div>
+  
+<nav class="navbar navbar-expand-sm shadow  navbar-inverse navbar-fixed-top  navbar-dark " style="background-color:#F5FCFF">
+
+<a class="nav-link text-black float-start text-warning fs-2 ps-4" href="">  <i class="fa-regular fa-user"></i> <?php echo  $_SESSION['name'] ?> </a>
+
+<img src="images/logo1.png"  class="d-block mx-auto" width="120px" height = "10%" >
+<a class="nav-link text-black float-end text-warning fs-3" href="logout.php">  <i class="fa-solid fa-arrow-right-from-bracket"></i></a>
+   
+</nav>
+</div>
