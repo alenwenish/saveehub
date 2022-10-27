7db7075c012872d12feb2e077ce12d39a4c6e5c8
SATHISHKUMAR-01 <sathishprakash233@gmail.com>
2022-10-27 22:18:24 +0530

    Home page post added

--- home.php
+++ home.php
@@ -3,6 +3,14 @@
 session_start();
 include('config/connect.php');
 
+
+$username = $_SESSION['name'];
+$sql = "SELECT * FROM image where username != '$username'";
+$res = mysqli_query($conn,$sql);
+$rows = array();
+while($row = mysqli_fetch_array($res))
+    $rows[] = $row;
+
 ?>
 
 <!DOCTYPE html>
@@ -17,11 +25,22 @@ include('config/connect.php');
 
 <body>
     <?php include('navigation.php'); ?>
+    <br>
 
-    <p class="text-center p-3"> No posts yet :(</p>
+    <?php foreach($rows as $row) { ?>
+
+    <div class="text-center">
+       <h4 class="ps-3"> <i class="fa-regular fa-user"></i> <?php echo $row['username']; ?>  </h4>
+       <img src="./uploads/<?php echo $row['post']; ?>"  width="300px" class="p-3 shadow mx-auto" alt="">
+       <h4 class="p-1"> <i class="fa-regular fa-heart"> </i> <?php echo $row['likes']; ?> likes  </h4>
+    </div>
+    <hr>
+   
+    <?php } ?>
 
     
 
+    <br><br>
     <?php include('footer.php'); ?>
 </body>
 </html>
