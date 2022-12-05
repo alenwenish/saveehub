ac697d035b69898c3404cfe1943bdb306caa9abf
SATHISHKUMAR-01 <sathishprakash233@gmail.com>
2022-12-05 14:12:55 +0530

    Events page updated

--- events.php
+++ events.php
@@ -38,15 +38,37 @@ while ($row = mysqli_fetch_array($res))
     <?php foreach ($rows as $row) { ?>
 
         <div class="text-center">
-            <h4 class="ps-3"> <i class="fa-regular fa-user"></i> <?php echo $row['username']; ?> </h4>
+            <h4 class="ps-3">
+                <i class="fa-regular fa-user"></i> <?php echo $row['username']; ?>
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                <span>
+                    <i class="fa-solid fa-ellipsis-vertical btn" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="Save Post">
+                    </i>
+                </span>
+            </h4>
             <img src="./club_pics/<?php echo $row['post']; ?>" width="300px" class="p-3 shadow mx-auto" alt="">
-            <h4 class="p-1"> <a href="update.php?id=<?php echo $row['id'] ?>&pic=0" class="text-decoration-none text-danger"> <i class="fa-regular fa-heart"> </i> </a> <?php echo $row['likes']; ?> likes </h4>
+            <h4 class="p-1">
+                <a href="update.php?id=<?php echo $row['id'] ?>&pic=0" class="text-decoration-none text-danger">
+                    <i class="fa-regular fa-heart"> </i>
+                </a>
+                <?php echo $row['likes']; ?> likes
+                &nbsp;
+                <i class="fa-regular fa-comment text-primary"></i>
+                <span> 0 Comments</span>
+            </h4>
         </div>
         <hr>
 
     <?php } ?>
 
     <br><br>
+
+    <script>
+        var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+        var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+            return new bootstrap.Popover(popoverTriggerEl)
+        })
+    </script>
 
 
 
