bc502a3a9e702f5d228c07a48f670bf854ecab04
SATHISHKUMAR-01 <sathishprakash233@gmail.com>
2022-12-05 14:09:36 +0530

    Comments, Save post button added in home page

--- home.php
+++ home.php
@@ -29,6 +29,14 @@ while ($row = mysqli_fetch_array($res))
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - Page</title>
     <?php include('header.php') ?>
+
+    <style>
+        .friends_pic {
+            width: 40px;
+            height: 40px;
+            border-radius: 80px;
+        }
+    </style>
 </head>
 
 <body>
@@ -40,16 +48,53 @@ while ($row = mysqli_fetch_array($res))
         <div class="text-center">
 
 
-            <h4 class="ps-3"> <i class="fa-regular fa-user"></i> <?php echo $row['username']; ?> </h4>
+            <h4 class="ps-3">
+                <?php
+                $pic = '';
+                $name = $row['username'];
+                $sql = "SELECT pic FROM login where username = '$name'";
+                $res = mysqli_query($conn, $sql);
+                $pic = $res->fetch_array()['pic'];
+                if ($pic == '') { ?>
+                    <i class="fa-regular fa-user"></i>
+                <?php } else { ?>
+                    <img src="./profile_pics/<?php echo $pic; ?>" alt="" class="friends_pic">
+                <?php } ?>
+                <?php echo $row['username']; ?>
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                <span>
+                    <i class="fa-solid fa-ellipsis-vertical btn"
+                    data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="Save Post">
+                    </i>
+                </span>
+            </h4>
+
+
             <img src="./uploads/<?php echo $row['post']; ?>" width="300px" class="p-3 shadow mx-auto" alt="">
-            <h4 class="p-1"> <a href="update.php?id=<?php echo $row['id'] ?>&pic=1" class="text-decoration-none text-danger"> <i class="fa-regular fa-heart"> </i> </a> <?php echo $row['likes']; ?> likes </h4>
+            <h4 class="p-1">
+                <a href="update.php?id=<?php echo $row['id'] ?>&pic=1" class="text-decoration-none text-danger">
+                    <i class="fa-regular fa-heart"> </i>
+                </a>
+                <?php echo $row['likes']; ?> likes
+                &nbsp;
+                <i class="fa-regular fa-comment text-primary"></i>
+                <span> 0 Comments</span>
+            </h4>
         </div>
         <hr>
 
     <?php } ?>
 
+
     <br><br>
     <?php include('footer.php'); ?>
+
+    <script>
+        var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+        var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+            return new bootstrap.Popover(popoverTriggerEl)
+        })
+    </script>
 
 
 </body>
