4518e8bdb25dcfca869801776617bb146e9104f0
SATHISHKUMAR-01 <sathishprakash233@gmail.com>
2022-12-11 15:15:03 +0530

    Delete button for comments in profile

--- comments.php
+++ comments.php
@@ -30,6 +30,7 @@ if (isset($_POST['post'])) {
 }
 
 $rows = array();
+$row_of_pic = array();
 
 if ($status_of_comment == 0) {
     $sql = "SELECT * FROM image_comments where image_id = '$id'";
@@ -37,12 +38,26 @@ if ($status_of_comment == 0) {
 
     while ($row = mysqli_fetch_array($res))
         $rows[] = $row;
+
+    $query = "SELECT * from image where id = '$id'";
+    $result = mysqli_query($conn, $query);
+
+    while ($r = mysqli_fetch_array($result))
+        $row_of_pic[] = $r;
+
 } else if ($status_of_comment == 1) {
     $sql = "SELECT * FROM club_pics_comments where image_id = '$id'";
     $res = mysqli_query($conn, $sql);
 
     while ($row = mysqli_fetch_array($res))
         $rows[] = $row;
+
+
+    $query = "SELECT * from club_pics where id = '$id'";
+    $result = mysqli_query($conn, $query);
+
+    while ($r = mysqli_fetch_array($result))
+        $row_of_pic[] = $r;
 }
 
 
@@ -75,21 +90,12 @@ if ($status_of_comment == 0) {
 
         <div class="justify-content-center text-center m-2">
 
-            <?php
-            $query = "SELECT * from image where id = '$id'";
-            $result = mysqli_query($conn, $query);
-            $row = array();
-            while ($r = mysqli_fetch_array($result))
-                $row[] = $r;
-
-            ?>
-
             <a href="javascript:history.go(-1)" class="text-dark text-decoration-none fs-3 ms-5 float-start"><i class="fa-solid fa-arrow-left"></i> </a>
 
             <h4 class="ps-3 ">
                 <?php
                 $picture = '';
-                $name = $row[0]['username'];
+                $name = $row_of_pic[0]['username'];
                 $sql = "SELECT pic FROM login where username = '$name'";
                 $res = mysqli_query($conn, $sql);
                 $picture = $res->fetch_array()['pic'];
@@ -98,7 +104,7 @@ if ($status_of_comment == 0) {
                 <?php } else { ?>
                     <img src="./profile_pics/<?php echo $picture; ?>" alt="" class="friends_pic">
                 <?php } ?>
-                <?php echo $row[0]['username']; ?>
+                <?php echo $row_of_pic[0]['username']; ?>
                 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
                 <span>
                     <i class="fa-solid fa-ellipsis-vertical btn" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="Save Post">
@@ -107,20 +113,20 @@ if ($status_of_comment == 0) {
             </h4>
 
 
-            <img src="./uploads/<?php echo $row[0]['post']; ?>" width="400px" class="p-3 shadow mx-auto" alt="">
+            <img src="./uploads/<?php echo $row_of_pic[0]['post']; ?>" width="400px" class="p-3 shadow mx-auto" alt="">
 
             <br>
             <br>
             <h4 class="p-1">
-                <a href="update.php?id=<?php echo $row[0]['id'] ?>&pic=1" class="text-decoration-none text-danger">
+                <a href="update.php?id=<?php echo $row_of_pic[0]['id'] ?>&pic=1" class="text-decoration-none text-danger">
                     <i class="fa-regular fa-heart"> </i>
                 </a>
-                <?php echo $row[0]['likes']; ?> likes
+                <?php echo $row_of_pic[0]['likes']; ?> likes
                 &nbsp;
                 <i class="fa-regular fa-comment text-primary"></i>
-                <a href="comments.php?id=<?php echo $row[0]['id'] ?>&owner=<?php echo $row[0]['username']; ?>&comment_status=0" class="text-decoration-none text-secondary"><span>
+                <a href="comments.php?id=<?php echo $row_of_pic[0]['id'] ?>&owner=<?php echo $row_of_pic[0]['username']; ?>&comment_status=0" class="text-decoration-none text-secondary"><span>
                         <?php
-                        $image_id = $row[0]['id'];
+                        $image_id = $row_of_pic[0]['id'];
                         $comment_query = "SELECT COUNT(comments)as count from image_comments where image_id='$image_id'";
                         $res = mysqli_query($conn, $comment_query);
 
@@ -131,16 +137,16 @@ if ($status_of_comment == 0) {
 
             </h4>
 
-            <?php if ($row[0]['caption'] != '') {  ?>
+            <?php if ($row_of_pic[0]['caption'] != '') {  ?>
                 <div class="text-start fs-5 fw-light mx-auto" style="width: 300px; height:auto;">
-                    <span class="fw-bold"> <?php echo $row[0]['username']; ?>:</span> <?php echo $row[0]['caption'] ?>
+                    <span class="fw-bold"> <?php echo $row_of_pic[0]['username']; ?>:</span> <?php echo $row_of_pic[0]['caption'] ?>
                 </div>
             <?php } ?>
 
             <div class="text-start text-secondary mx-auto" style="width: 300px; height:auto;">
                 <span class="fs-6">
                     <?php
-                    $dt = strtotime($row[0]['created_at']);
+                    $dt = strtotime($row_of_pic[0]['created_at']);
                     echo date("d", $dt);
                     echo " ";
                     echo date("M", $dt);
@@ -213,7 +219,7 @@ if ($status_of_comment == 0) {
                             ?>
                         </span>
 
-                        <?php if ($username == $row['commenter']) { ?>
+                        <?php if ($username == $row['commenter'] || $username == $row_of_pic[0]['username']) { ?>
                             <button type="submit" class="btn" name="delete_comment_user">
                                 <i class="fa-solid fa-trash fs-6 text-danger"></i>
                             </button>
@@ -233,21 +239,13 @@ if ($status_of_comment == 0) {
 
         <div class="justify-content-center text-center m-2">
 
-            <?php
-            $query = "SELECT * from club_pics where id = '$id'";
-            $result = mysqli_query($conn, $query);
-            $row = array();
-            while ($r = mysqli_fetch_array($result))
-                $row[] = $r;
-
-            ?>
 
             <a href="javascript:history.go(-1)" class="text-dark text-decoration-none fs-3 ms-5 float-start"><i class="fa-solid fa-arrow-left"></i> </a>
 
             <h4 class="ps-3 ">
                 <?php
                 $picture = '';
-                $name = $row[0]['username'];
+                $name = $row_of_pic[0]['username'];
                 $sql = "SELECT pic FROM club where club_name = '$name'";
                 $res = mysqli_query($conn, $sql);
                 $picture = $res->fetch_array()['pic'];
@@ -256,7 +254,7 @@ if ($status_of_comment == 0) {
                 <?php } else { ?>
                     <img src="./profile_pics/<?php echo $picture; ?>" alt="" class="friends_pic">
                 <?php } ?>
-                <?php echo $row[0]['username']; ?>
+                <?php echo $row_of_pic[0]['username']; ?>
                 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
                 <span>
                     <i class="fa-solid fa-ellipsis-vertical btn" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="Save Post">
@@ -265,20 +263,20 @@ if ($status_of_comment == 0) {
             </h4>
 
 
-            <img src="./club_pics/<?php echo $row[0]['post']; ?>" width="400px" class="p-3 shadow mx-auto" alt="">
+            <img src="./club_pics/<?php echo $row_of_pic[0]['post']; ?>" width="400px" class="p-3 shadow mx-auto" alt="">
 
             <br>
             <br>
             <h4 class="p-1">
-                <a href="update.php?id=<?php echo $row[0]['id'] ?>&pic=1" class="text-decoration-none text-danger">
+                <a href="update.php?id=<?php echo $row_of_pic[0]['id'] ?>&pic=1" class="text-decoration-none text-danger">
                     <i class="fa-regular fa-heart"> </i>
                 </a>
-                <?php echo $row[0]['likes']; ?> likes
+                <?php echo $row_of_pic[0]['likes']; ?> likes
                 &nbsp;
                 <i class="fa-regular fa-comment text-primary"></i>
-                <a href="comments.php?id=<?php echo $row[0]['id'] ?>&owner=<?php echo $row[0]['username']; ?>&comment_status=1" class="text-decoration-none text-secondary"><span>
+                <a href="comments.php?id=<?php echo $row_of_pic[0]['id'] ?>&owner=<?php echo $row_of_pic[0]['username']; ?>&comment_status=1" class="text-decoration-none text-secondary"><span>
                         <?php
-                        $image_id = $row[0]['id'];
+                        $image_id = $row_of_pic[0]['id'];
                         $comment_query = "SELECT COUNT(comments)as count from club_pics_comments where image_id='$image_id'";
                         $res = mysqli_query($conn, $comment_query);
 
@@ -289,16 +287,16 @@ if ($status_of_comment == 0) {
 
             </h4>
 
-            <?php if ($row[0]['caption'] != '') {  ?>
+            <?php if ($row_of_pic[0]['caption'] != '') {  ?>
                 <div class="text-start fs-5 fw-light mx-auto" style="width: 300px; height:auto;">
-                    <span class="fw-bold"> <?php echo $row[0]['username']; ?>:</span> <?php echo $row[0]['caption'] ?>
+                    <span class="fw-bold"> <?php echo $row_of_pic[0]['username']; ?>:</span> <?php echo $row_of_pic[0]['caption'] ?>
                 </div>
             <?php } ?>
 
             <div class="text-start text-secondary mx-auto" style="width: 300px; height:auto;">
                 <span class="fs-6">
                     <?php
-                    $dt = strtotime($row[0]['created_at']);
+                    $dt = strtotime($row_of_pic[0]['created_at']);
                     echo date("d", $dt);
                     echo " ";
                     echo date("M", $dt);
@@ -371,7 +369,7 @@ if ($status_of_comment == 0) {
                             ?>
                         </span>
 
-                        <?php if ($username == $row['commenter']) { ?>
+                        <?php if ($username == $row['commenter']  || $username == $row_of_pic[0]['username'] ) { ?>
                             <button type="submit" class="btn" name="delete_comment_club">
                                 <i class="fa-solid fa-trash fs-6 text-danger"></i>
                             </button>
