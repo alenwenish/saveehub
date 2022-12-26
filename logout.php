06bf239f90e8c70ea14b9cee49761cc3ea1213dc
SATHISHKUMAR-01 <sathishprakash233@gmail.com>
2022-12-26 20:06:00 +0530

    Undone Commites

--- logout.php
+++ logout.php
@@ -5,14 +5,3 @@ if(session_destroy()) {
     header("Location: login.php");
 }
 ?>
-
-
-
-<form action="" method="POST" class="d-flex mx-auto justify-content-center w-75">
-    <div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Search here... " name="search_name"
-            aria-describedby="button-addon2">
-        <button class="btn btn-primary" type="submit" name="search" id="button-addon2"><i
-                class="fa-solid fa-magnifying-glass"></i></button>
-    </div>
-</form>
